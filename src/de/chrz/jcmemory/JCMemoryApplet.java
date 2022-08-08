7b0648f4ac81990d5f5f39e6cbf3dc30f35ac6ca
Christoph Honal <christoph.honal@web.de>
2022-08-08 02:28:47 +0200

    Use fidesmo apdu

--- src/de/chrz/jcmemory/JCMemoryApplet.java
+++ src/de/chrz/jcmemory/JCMemoryApplet.java
@@ -9,43 +9,43 @@ import javacard.framework.JCSystem;
 public class JCMemoryApplet extends Applet
 {
 
-	public static void
-	install(byte[] info, short off, byte len)
-	{
-		final JCMemoryApplet applet = new JCMemoryApplet();
-		applet.register();
-	}
+    public static void
+    install(byte[] info, short off, byte len)
+    {
+        final JCMemoryApplet applet = new JCMemoryApplet();
+        applet.register();
+    }
 
-	protected
-	JCMemoryApplet()
-	{
+    protected
+    JCMemoryApplet()
+    {
 
-	}
+    }
 
-	public void
-	process(APDU apdu)
-	{
-		final byte[] buffer = apdu.getBuffer();
-		final byte ins = buffer[ISO7816.OFFSET_INS];
+    public void
+    process(APDU apdu)
+    {
+        final byte[] buffer = apdu.getBuffer();
+        final byte ins = buffer[ISO7816.OFFSET_INS];
 
-		if (!apdu.isISOInterindustryCLA()) {
-			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
-			return;
-		}
+        if (!apdu.isISOInterindustryCLA()) {
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+            return;
+        }
 
-		if (selectingApplet()) {
+        if (selectingApplet()) {
             short len, le;
             le = apdu.setOutgoing();
             len = 0;
 
-			JCSystem.requestObjectDeletion();
+            JCSystem.requestObjectDeletion();
             short[] memPer = new short[2];
-			JCSystem.getAvailableMemory(memPer, (short)0, JCSystem.MEMORY_TYPE_PERSISTENT);
+            JCSystem.getAvailableMemory(memPer, (short)0, JCSystem.MEMORY_TYPE_PERSISTENT);
             short memTRes = JCSystem.getAvailableMemory(JCSystem.MEMORY_TYPE_TRANSIENT_RESET);
             short memTDes = JCSystem.getAvailableMemory(JCSystem.MEMORY_TYPE_TRANSIENT_DESELECT);
             buffer[len++] = (byte)(memPer[0] & 0xFF);
             buffer[len++] = (byte)((memPer[0] >> 8) & 0xFF);
-			buffer[len++] = (byte)(memPer[1] & 0xFF);
+            buffer[len++] = (byte)(memPer[1] & 0xFF);
             buffer[len++] = (byte)((memPer[1] >> 8) & 0xFF);
             buffer[len++] = (byte)(memTRes & 0xFF);
             buffer[len++] = (byte)((memTRes >> 8) & 0xFF);
@@ -56,8 +56,8 @@ public class JCMemoryApplet extends Applet
             apdu.setOutgoingLength(len);
             apdu.sendBytes((short)0, len);
 
-			return;
-		}
-	}
+            return;
+        }
+    }
 
 }
