e4269c1b5233c266cae6ca9546ff83985acc963a
Christoph Honal <christoph.honal@web.de>
2023-02-07 04:52:15 +0100

    Reduce memory wear

--- src/de/chrz/jcmemory/JCMemoryApplet.java
+++ src/de/chrz/jcmemory/JCMemoryApplet.java
@@ -10,10 +10,10 @@ import javacard.framework.Util;
 public class JCMemoryApplet extends Applet
 {
 
-    private short[] memPerTot;
+    private static short[] memPerTot;
+    private static short[] memPer;
 
-    public static void
-    install(byte bArray[], short bOffset, byte bLength) throws ISOException
+    public static void install(byte bArray[], short bOffset, byte bLength) throws ISOException
     {
         short offset = bOffset;
         offset += (short)(bArray[offset] + 1); // Instance
@@ -21,10 +21,10 @@ public class JCMemoryApplet extends Applet
         new JCMemoryApplet(bArray, (short)(offset + 1), bArray[offset]).register(bArray, (short)(bOffset + 1), bArray[bOffset]);
     }
 
-    protected
-    JCMemoryApplet(byte[] parameters, short parametersOffset, byte parametersLength)
+    protected JCMemoryApplet(byte[] parameters, short parametersOffset, byte parametersLength)
     {
         memPerTot = new short[2];
+        memPer = JCSystem.makeTransientShortArray((short) 2, JCSystem.CLEAR_ON_DESELECT);
         if (parametersLength >= 4) {
             memPerTot[0] = Util.getShort(parameters, parametersOffset);
             memPerTot[1] = Util.getShort(parameters, (short)(parametersOffset + 2));
@@ -35,8 +35,7 @@ public class JCMemoryApplet extends Applet
         }
     }
 
-    public void
-    process(APDU apdu)
+    public void process(APDU apdu)
     {
         final byte[] buffer = apdu.getBuffer();
         final byte ins = buffer[ISO7816.OFFSET_INS];
@@ -52,7 +51,6 @@ public class JCMemoryApplet extends Applet
             len = 0;
 
             JCSystem.requestObjectDeletion();
-            short[] memPer = new short[2];
             JCSystem.getAvailableMemory(memPer, (short)0, JCSystem.MEMORY_TYPE_PERSISTENT);
             short memTRes = JCSystem.getAvailableMemory(JCSystem.MEMORY_TYPE_TRANSIENT_RESET);
             short memTDes = JCSystem.getAvailableMemory(JCSystem.MEMORY_TYPE_TRANSIENT_DESELECT);
@@ -66,7 +64,7 @@ public class JCMemoryApplet extends Applet
 
             len = le > 0 ? (le > len ? len : le) : len;
             apdu.setOutgoingLength(len);
-            apdu.sendBytes((short)0, len);
+            apdu.sendBytes((short) 0, len);
 
             return;
         }
